f372fa1a6a7089567fd93399a449bcf83032eadf
ivanov <maxim2003.maks@mail.ru>
2024-03-03 23:30:37 +0300

    Multimodule Maven project Calculator complete. Ivanov

---
+++ calculator/src/main/java/ru/calculator/ivanov/Calc.java
@@ -0,0 +1,7 @@
+package ru.calculator.ivanov;
+
+public class Calc {
+    public int addition (int x, int y) {
+        return x + y;
+    }
+}
---
+++ main/src/main/java/ru/calculator/ivanov/Main.java
@@ -0,0 +1,9 @@
+package ru.calculator.ivanov;
+
+public class Main {
+    public static void main( String[] args ) {
+        Calc c = new Calc();
+
+        System.out.println(c.addition(5, 5));
+    }
+}
